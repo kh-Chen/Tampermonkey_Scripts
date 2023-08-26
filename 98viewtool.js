2c329ff976d985d564f138dd883eb52dd38ee550
kh-Chen <42796801+kh-Chen@users.noreply.github.com>
2023-08-26 11:48:39 +0800

    Update 98viewtool.js

--- 98viewtool.js
+++ 98viewtool.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         98图片预览助手
 // @namespace    98imgloader
-// @version      1.0.3
+// @version      1.0.5
 // @description  浏览帖子列表时自动加载内部前三张图片供预览。如需支持免翻地址，请使用@match自行添加连接，如果某个版块不希望预览，请使用@exclude自行添加要排除的版块链接
 // @author       sehuatang_chen
 // @license      MIT
@@ -10,8 +10,8 @@
 // @match        https://*sehuatang.net/forum.php?mod=forumdisplay*
 // @match        https://*mzjvl.com/forum.php?mod=forumdisplay*
 
-// @grant          GM_xmlhttpRequest
-// @require https://code.jquery.com/jquery-3.6.1.min.js
+// @grant        GM_xmlhttpRequest
+// @require      https://code.jquery.com/jquery-3.6.1.min.js
 
 // ==/UserScript==
 /* global $ */
@@ -26,19 +26,19 @@ $(document).ready(function(){
     $("div.show-text2").parent().parent().parent().remove();
     $("tbody[id='separatorline']").remove();
 
-    function do_load_img(){
+    function do_load_thread_info(){
         $("tbody[id*='normalthread']").each(function(){
             var tbody=$(this);
-            var tbody_img_id = "img_"+tbody.attr("id").split("_")[1];
-            if ($("#"+tbody_img_id).length != 0) {
+            var tbody_clone_id = "info_"+tbody.attr("id").split("_")[1];
+            if ($("#"+tbody_clone_id).length != 0) {
                 return;
             }
 
-            var tbody_img = tbody.clone();
-            tbody_img.attr("id", tbody_img_id);
-            tbody_img.find("td,th").remove();
-            var td_img = $('<td colspan="5"></td>');
-            tbody_img.find("tr:eq(0)").append(td_img);
+            var tbody_clone = tbody.clone();
+            tbody_clone.attr("id", tbody_clone_id);
+            tbody_clone.find("td,th").remove();
+            var tag_td = $('<td colspan="5"></td>');
+            tbody_clone.find("tr:eq(0)").append(tag_td);
 
             var url = tbody.find(".icn > a").attr("href");
             GM_xmlhttpRequest({
@@ -50,38 +50,76 @@ $(document).ready(function(){
                         return ;
                     }
                     var doc = result.responseText;
-                    $(doc).find(".zoom").slice(0, 3).each(function() {
-                        var zoomfile = $(this).attr("zoomfile")
-                        //console.log(zoomfile)
-                        if (zoomfile != undefined && zoomfile != null && zoomfile != '') {
-                            var tag_img = $('<img>');
-                            tag_img.attr('src', zoomfile);
-                            tag_img.css({
-                                "max-width": "33%",
-                                "max-height": "300px",
-                            });
-
-                            td_img.append(tag_img);
-                        }
-
-                    });
+                    load_img(doc,tag_td)
+                    //为防止拿到资源后不点进帖子导致不评分评论收藏等，默认关闭此功能
+                    //如果你看见了这个功能并想使用，不要声张，打开注释即可。
+                    //load_download_link(doc,tag_td)
                 }
             });
-            tbody.after(tbody_img);
+            tbody.after(tbody_clone);
         })
+    }
+
+    function load_img(doc,con){
+        var tag_div = $('<div></div>');
+        tag_div.css("margin-bottom","5px");
+        $(doc).find(".zoom").slice(0, 3).each(function() {
+            var zoomfile = $(this).attr("zoomfile")
+            if (zoomfile != undefined && zoomfile != null && zoomfile != '') {
+                var tag_img = $('<img>');
+                tag_img.attr('src', zoomfile);
+                tag_img.css({
+                    "max-width": "33%",
+                    "max-height": "300px",
+                });
+
+                tag_div.append(tag_img);
+            }
+        });
+        if (tag_div.children().length == 0) {
+            tag_div.append("未识别到图片")
+        }
+        con.append(tag_div);
+    }
+
+    function load_download_link(doc,con){
+        var tag_div = $('<div></div>');
+        var _doc = $(doc);
+        _doc.find("span[id*='attach_']").each(function() {
+            var attach = $(this)
+            if (attach.find("a").length > 0){
+                tag_div.append(attach.parent().clone())
+            }
+        });
+        _doc.find("dl.tattl").each(function() {
+            var attach = $(this);
+            if (attach.find("p.attnm").length > 0){
+                tag_div.append(attach.parent().clone())
+            }
+        });
+        _doc.find("div.blockcode").each(function() {
+            var codediv = $(this)
+            codediv.find("li").each(function(){
+                tag_div.append('<div>'+$(this).text()+'</div>')
+            })
+        });
+        if (tag_div.children().length == 0) {
+            tag_div.append("未识别到资源连接")
+        }
+        con.append(tag_div);
     }
 
     var next_btn = $("#autopbn");
     next_btn.on("click",function(){
         console.log("next_btn click!!!")
-        //点击下一页后延迟1秒再次触发图片加载。
-        setTimeout(do_load_img, 1000);
+        //点击下一页后延迟1.5秒再次触发图片加载。
+        setTimeout(do_load_thread_info, 1500);
     })
 
     // $("#threadlisttableid").bind("DOMNodeInserted", function(){
     //     console.log("DOMNodeInserted!!!")
     // });
-    do_load_img()
+    do_load_thread_info()
 
 
 });
