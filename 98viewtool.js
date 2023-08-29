14d887dac33661acbe39d9a745bcd28ba808435a
kh-Chen <18241224605@163.com>
2023-08-29 19:36:18 +0800

    fix bug

--- 98viewtool.js
+++ 98viewtool.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         98图片预览助手
 // @namespace    98imgloader
-// @version      1.3.2
+// @version      1.3.3
 // @description  浏览帖子列表时自动加载内部前三张(可配置)图片供预览。如需支持其他免翻地址，请使用@match自行添加连接，如果某个版块不希望预览，请使用@exclude自行添加要排除的版块链接
 // @author       sehuatang_chen
 // @license      MIT
@@ -192,7 +192,7 @@ const imgs = {
     hide_svg  : '<svg viewBox="0 0 1024 1024" style="width:16px;height=16px;cursor:pointer" xmlns="http://www.w3.org/2000/svg" data-v-ea893728=""><path d="M876.8 156.8c0-9.6-3.2-16-9.6-22.4-6.4-6.4-12.8-9.6-22.4-9.6-9.6 0-16 3.2-22.4 9.6L736 220.8c-64-32-137.6-51.2-224-60.8-160 16-288 73.6-377.6 176C44.8 438.4 0 496 0 512s48 73.6 134.4 176c22.4 25.6 44.8 48 73.6 67.2l-86.4 89.6c-6.4 6.4-9.6 12.8-9.6 22.4 0 9.6 3.2 16 9.6 22.4 6.4 6.4 12.8 9.6 22.4 9.6 9.6 0 16-3.2 22.4-9.6l704-710.4c3.2-6.4 6.4-12.8 6.4-22.4Zm-646.4 528c-76.8-70.4-128-128-153.6-172.8 28.8-48 80-105.6 153.6-172.8C304 272 400 230.4 512 224c64 3.2 124.8 19.2 176 44.8l-54.4 54.4C598.4 300.8 560 288 512 288c-64 0-115.2 22.4-160 64s-64 96-64 160c0 48 12.8 89.6 35.2 124.8L256 707.2c-9.6-6.4-19.2-16-25.6-22.4Zm140.8-96c-12.8-22.4-19.2-48-19.2-76.8 0-44.8 16-83.2 48-112 32-28.8 67.2-48 112-48 28.8 0 54.4 6.4 73.6 19.2L371.2 588.8ZM889.599 336c-12.8-16-28.8-28.8-41.6-41.6l-48 48c73.6 67.2 124.8 124.8 150.4 169.6-28.8 48-80 105.6-153.6 172.8-73.6 67.2-172.8 108.8-284.8 115.2-51.2-3.2-99.2-12.8-140.8-28.8l-48 48c57.6 22.4 118.4 38.4 188.8 44.8 160-16 288-73.6 377.6-176C979.199 585.6 1024 528 1024 512s-48.001-73.6-134.401-176Z" fill="currentColor"></path><path d="M511.998 672c-12.8 0-25.6-3.2-38.4-6.4l-51.2 51.2c28.8 12.8 57.6 19.2 89.6 19.2 64 0 115.2-22.4 160-64 41.6-41.6 64-96 64-160 0-32-6.4-64-19.2-89.6l-51.2 51.2c3.2 12.8 6.4 25.6 6.4 38.4 0 44.8-16 83.2-48 112-32 28.8-67.2 48-112 48Z" fill="currentColor"></path></svg>',
 }
 const tools = {
-    base_selector: "#postlist > div[id^=post_] div.pct:eq(0) div.t_fsz ",
+    base_selector: "#postlist > div[id^=post_]:eq(0) ",
     headers: {
         'User-agent': navigator.userAgent,
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -222,10 +222,12 @@ const tools = {
             "max-height"    :(GM_getValue("img_max_height")+10) + "px",
         });
         $to_con.append($tag_div);
-        var $inner_tag_div = $('<div style="display: flex;align-items: flex-start;"></div>');
-
-        $from_con.find("img.zoom[aid]")
-            .slice(0, parseInt(GM_getValue("img_max_count")))
+        var $inner_tag_div = $('<div style="display: flex;align-items: center;"></div>');
+        var $imgs = $from_con.find("img.zoom[aid]")
+        if ($imgs.length >= parseInt(GM_getValue("img_max_count"))) {
+            $inner_tag_div.append('<span style="writing-mode: vertical-lr;">共'+$imgs.length+'张图</span>')
+        }
+        $imgs.slice(0, parseInt(GM_getValue("img_max_count")))
             .each(function(){
                 var pic_url = $(this).attr( "zoomfile" ) || $(this).attr( "file" ) || $(this).attr( "src" );
                 if (pic_url != undefined && pic_url != null && pic_url != '') {
@@ -456,7 +458,7 @@ const GM_script = {
                 </div>
             `);
             $btns.find("#confirm_btn").click(function(){
-                console.log("config value:");show_hide_btn
+                console.log("config value:");
                 console.log("show_hide_btn", $form.find("#show_hide_btn").prop("checked"));
                 GM_setValue("show_hide_btn", $form.find("#show_hide_btn").prop("checked") ? 1 : 0);
                 console.log("switch_autoload", $form.find("#switch_autoload").prop("checked"));
